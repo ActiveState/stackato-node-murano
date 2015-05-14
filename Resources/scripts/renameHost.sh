1eeb348646f220f053290be816fc75e542be04dd
bcferrycoder <bcferrycoder@bcferrycoder.com>
2015-05-14 11:06:03 -0700

    remove logging, add comments

--- Resources/scripts/renameHost.sh
+++ Resources/scripts/renameHost.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 # $1 - IP address of the node
 
+# add stackato to sudoers so rename doesn't prompt for pw
 chmod u+w /etc/sudoers
 echo "stackato        ALL=(ALL)       NOPASSWD: ALL" >> /etc/sudoers
 chmod u-w /etc/sudoers
 
-echo "**** time: `date` | user: `/usr/bin/whoami`" >> /tmp/setup.log
-echo "***** ready?" >> /tmp/setup.log
+# wait for stackato to boot
 sudo su -l stackato -c "/home/stackato/bin/kato process ready --block 1000 all" >> /tmp/setup.log 2>&1
-echo "**** time: `date` | user: `/usr/bin/whoami`" >> /tmp/setup.log
-echo "***** rename" >> /tmp/setup.log
+
+# rename host to xip.io name
 sudo su -l stackato -c "/home/stackato/bin/kato node rename $1.xip.io" >> /tmp/setup.log 2>&1
-echo "**** time: `date` | hostname: `/bin/hostname`" >> /tmp/setup.log
