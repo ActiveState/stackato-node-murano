a005b8ba1739a8f314728e8279fff86e778780b4
bcferrycoder <bcferrycoder@bcferrycoder.com>
2015-05-14 11:35:09 -0700

    move sudoers entry to sudoers.d/murano

--- Resources/scripts/renameHost.sh
+++ Resources/scripts/renameHost.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 # $1 - IP address of the node
 
-# add stackato to sudoers so rename doesn't prompt for pw
-chmod u+w /etc/sudoers
-echo "stackato        ALL=(ALL)       NOPASSWD: ALL" >> /etc/sudoers
-chmod u-w /etc/sudoers
+# disable pw check so rename doesn't prompt for pw
+#chmod u+w /etc/sudoers
+echo "stackato        ALL=(ALL)       NOPASSWD: ALL" >> /etc/sudoers.d/murano
+#chmod u-w /etc/sudoers
+
+# re-enable pw check for safety
+rm -f /etc/sudoers.d/murano
 
 # wait for stackato to boot
 sudo su -l stackato -c "/home/stackato/bin/kato process ready --block 1000 all" >> /tmp/setup.log 2>&1
